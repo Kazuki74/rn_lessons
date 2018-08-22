d4e75560df8391a344d952a6b6de5234d95bdac4
小林和希 <kobayashi.kazuki74@gmail.com>
2018-08-22 13:26:50 +0900

    データの取得と反映、スタイルの調整

--- App.js
+++ App.js
@@ -1,23 +1,73 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Image, StyleSheet, Text, View, FlatList, Dimensions } from 'react-native';
 
 export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      // 記事データを入れるための配列
+      threads: []
+    }
+  }
+  componentDidMount() {
+    fetch("https://www.reddit.com/r/newsokur/hot.json")
+      .then((response) => response.json())
+      .then((responseJson) => {
+        let threads = responseJson.data.children
+        threads = threads.map(i =>{
+          i.key = i.data.url
+          return i
+        })
+        this.setState({threads})
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
   render() {
+    const { threads } = this.state
+    // Dimensionsから画面幅を取得
+    const { width } = Dimensions.get('window')
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!!!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <React.Fragment>
+        <View style={styles.list}>
+          <FlatList data={ threads } renderItem={ ({item}) => {
+            return (
+              <View style={styles.items}>
+                <Image style={styles.image} source={{uri: item.data.thumbnail}}/>
+                <View style={{ width: width - 50 }}>
+                  <Text>{item.data.title}</Text>
+                  <Text style= {styles.domain}>{item.data.domain}</Text>
+                </View>
+              </View>
+            )
+          }} />
+        </View>
+      </React.Fragment>
     );
   }
 }
 
+// -の代わりにキャメルケース
+// 基本の単位はdp
 const styles = StyleSheet.create({
-  container: {
+  list: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  items: {
+    flex: 1,
+    flexDirection: 'row',
+    width: '100%',
+  },
+  image: {
+    // 画像表示には幅と高さの指定が必要
+    width: 50,
+    height: 50,
+  },
+  domain: {
+    color: '#ababab',
+    fontSize: 10,
   },
 });
